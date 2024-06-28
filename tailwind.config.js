596b35407d35ee3e840509b77022f166a33fdc94
Syauqi oqi <msyauqioqi@gmail.com>
2024-06-28 07:35:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./main/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        clv_comp: ["clv_comp"],
+        clv_cond: ["clv_cond"],
+      },
+      colors: {
+        primary: "#070F2B",
+        secondary: "#9290C3",
+        ourwhite: "#EEEEEE",
+      },
+      fontSize: {},
+      letterSpacing: {
+        tightest: "-.075em",
+        tighter: "-.05em",
+        tight: "-.025em",
+        normal: "0",
+        wide: ".025em",
+        wider: ".05em",
+        widest: ".1em",
+        widest: ".282em",
+      },
+    },
+  },
+  plugins: [],
+};
